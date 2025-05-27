7c90cae01c8fcc02de7d25eb82e3d9540e95bd1e
SSInamam <siiroedgar17@gmail.com>
2025-05-27 16:28:09 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_ZUNA UGARTE_J/0_PC _3/0_PC _3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desarrollar un código en el que almacenemos el nombre en una variable 'nombre' y la edad en la variable 'edad'  Los datos deben ser ingresados por el usuariosFinalmente debe saludar "Hola NOMBRE , tu edad es EDAD".
+            string nombre;
+            int edad;
+
+            Console.Write("cual es tu nombre :");
+
+            nombre = Console.ReadLine();
+
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("cual es tu edad :");
+            Console.WriteLine("hola mundo " + nombre + " tuedad es " + edad);
+
+            Console.WriteLine("tu nombre es " + nombre);
+
+            Console.ReadKey();
+
+        }
+    }
+}
