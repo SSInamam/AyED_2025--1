0286bc47eae6fdcfe938b331bb45fdd44a6adc86
SSInamam <siiroedgar17@gmail.com>
2025-05-27 16:31:10 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_ZUNA UGARTE_J/1_PC1_3/1_PC1_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cifra;
+
+            Console.Write(" ingrese un numerto :");
+            cifra = int.Parse(Console.ReadLine());
+
+            if (cifra % 2 == 1)
+            {
+                Console.WriteLine(cifra + "  tu numero es impar");
+
+            }
+            else if (cifra == 0)
+            {
+                Console.WriteLine(" tu numero es nulo ");
+            }
+
+            else
+            {
+                Console.WriteLine(cifra + "  tu numero es par"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
