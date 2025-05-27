e530839e20b1f6a522c3fea8f626b363e8eb4067
SSInamam <siiroedgar17@gmail.com>
2025-05-27 15:13:17 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_MAMANI SILVA_M/0_PC1_8/0_PC1_8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC1_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varI;
+            int varII;
+            int varRESTO;
+
+            varI = int.Parse(Console.ReadLine());
+            varII = int.Parse(Console.ReadLine());
+
+            varRESTO = varI % varII;
+
+            Console.WriteLine("el resto del resultado es : " + varRESTO);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 1_TRABAJOS_M&J/1_MAMANI SILVA_M/0_PC1_9/0_PC1_9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC1_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double varA;
+            double varb;
+            double varsuper;
+            double varvolum;
+
+            varA = int.Parse(Console.ReadLine());
+
+            varsuper = 4 * Math.PI * Math.Pow(varA, 2);
+
+
+
+            Console.WriteLine("LA SUPERFICIE DE TU ESFERA ES : " + varsuper);
+
+            varb = int.Parse(Console.ReadLine());
+
+            varvolum = Math.PI * Math.Pow(varb, 3) * 4/3;
+
+            Console.WriteLine("LA VOLUMEN DE TU ESFERA ES : " + varvolum);
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
