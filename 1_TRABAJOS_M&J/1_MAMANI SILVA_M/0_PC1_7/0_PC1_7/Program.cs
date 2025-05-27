3998d9aca80dd00f0f4aea073b4e76ba2ccf5d9f
SSInamam <siiroedgar17@gmail.com>
2025-05-27 15:12:14 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_MAMANI SILVA_M/0_PC1_7/0_PC1_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int varx;
+            int vary;
+            int varz;
+
+            varx = int.Parse(Console.ReadLine());
+            vary = int.Parse(Console.ReadLine());
+
+            varz = varx / vary;
+
+            Console.WriteLine("tu resultado es = " + varz);
+            Console.ReadKey();
+
+
+        }
+    }
+}
