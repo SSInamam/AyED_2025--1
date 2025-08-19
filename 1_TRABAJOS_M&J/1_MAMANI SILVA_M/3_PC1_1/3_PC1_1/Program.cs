ec574dc7db612e6b12b6368b6d6d7c4de6ed3830
SSInamam <siiroedgar17@gmail.com>
2025-08-19 16:52:50 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_MAMANI SILVA_M/3_PC1_1/3_PC1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC1_1
+{
+    class Program
+    {
+        static int valz(int valx, int valy)
+        {
+            return valx + valy;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(" Dame dos valores : ");
+            int valfin = valz(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine(" La suma de los valores es : " + valfin);
+            Console.ReadKey();
+        }
+    }
+}
