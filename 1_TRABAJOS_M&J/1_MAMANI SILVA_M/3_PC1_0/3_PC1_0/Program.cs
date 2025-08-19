f67a37696af649f793885fc959390b48d9c263c4
SSInamam <siiroedgar17@gmail.com>
2025-08-19 16:48:05 -0300

    Add files via upload

---
+++ 1_TRABAJOS_M&J/1_MAMANI SILVA_M/3_PC1_0/3_PC1_0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC1_0
+{
+    class Program
+    {
+
+        static string Conver(string saludo)
+        {
+            return "Hola "+ saludo +". Es un placer";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(" Muy Buenas, ¿ Cómo te llamas? : ");
+            string Resp = Conver (Console.ReadLine());
+            Console.WriteLine(Resp);
+
+            Console.ReadKey();
+        }
+    }
+}
